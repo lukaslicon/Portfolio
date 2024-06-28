a6df0470314f9b01e9eb8bdebef00e4fc000534e
Lukas Licon <82681042+lukaslicon@users.noreply.github.com>
2024-06-28 13:34:53 -0700

    styling changes

--- script.js
+++ script.js
@@ -9,29 +9,33 @@ const App = () => {
             case "Home":
                 return (
                     <div>
-                        <h2>Resume and Experiences</h2>
-                        <p>This section contains my resume and other experiences.</p>
-                    </div>
-                );
-            case "About":
-                return (
-                    <div>
                         <h2>About Me</h2>
-                        <p>This section contains information about Lukas Daniel Licon.</p>
+                        <p>This section is about who I am and what I do.</p>
                     </div>
                 );
             case "Projects":
                 return (
                     <div>
                         <h2>Projects</h2>
-                        <p>This section showcases various projects worked on by Lukas Daniel Licon.</p>
+                        <p>This section contains a showcase about my Projects.</p>
+                    </div>
+                );
+            case "Education":
+                return (
+                    <div>
+                        <h2>Education</h2>
+                        <p>This contains education from Lukas.</p>
                     </div>
                 );
             case "Contact":
                 return (
                     <div>
                         <h2>Contact</h2>
-                        <p>This section provides contact information for Lukas Daniel Licon.</p>
+                        <p>Feel free to contact me about any business inquiries.</p>
+                        <p><a href="mailto:lukas@licons.com">Send Email</a> </p>
+                        <p><a href="https://www.linkedin.com/in/lukas-licon/">LinkedIn</a></p>
+                        <p><a href="https://github.com/lukaslicon">Github</a></p>
+                        
                     </div>
                 );
             default:
@@ -43,13 +47,13 @@ const App = () => {
         <div className="container">
             <div className="header">
                 <h1>Lukas Daniel Licon</h1>
-                <p>Gameplay Programmer, Level Designer, Web Developer, Software Engineer</p>
+                <p>Gameplay Programmer, Game Designer, Web Developer, Software Engineer</p>
             </div>
             <div className="content-box">
                 <div className="navbar">
                     <button onClick={() => setActiveTab("Home")}>Home</button>
-                    <button onClick={() => setActiveTab("About")}>About</button>
                     <button onClick={() => setActiveTab("Projects")}>Projects</button>
+                    <button onClick={() => setActiveTab("Education")}>Education</button>
                     <button onClick={() => setActiveTab("Contact")}>Contact</button>
                 </div>
                 <div className="content">
