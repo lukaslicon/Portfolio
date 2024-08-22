ca35e68eff405fc7acd8a386fcb710912d912851
Lukas Licon <82681042+lukaslicon@users.noreply.github.com>
2024-08-22 14:59:12 -0700

    descriptions changed

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ const App = () => {
                         </div>
                         <h3>Crabity</h3>
                         <h5><a href="https://store.steampowered.com/app/2988850/Crabity/"> Link to Steam</a></h5>
+                        <h5><a href="https://github.com/Team-Crabity/Crabity"> Link to Github</a></h5>
                         <div className="gprojects-item">
                             <div className="gprojects-logo">
                                 <img src="assets/crabity screenshot.png" alt="Crabity Logo" className="gprojects-logo-img" />
@@ -62,12 +63,13 @@ const App = () => {
                                     <h4>Contributions</h4>
                                 </div>
                                 <div className="gprojects-contributions">
-                                    <p>-Created daily generated maps using weighted pathfinding and procedural generation based on movement.</p>
+                                    <p>Created daily generated maps using a weighted pathfinding algorithm to make corridors based on cell movement directions. Debugged gameplay mechanics & player movement. Implemetation of UI scripts and pause menu scripting.</p>
                                 </div>
                             </div>
                         </div>
                         <h3>Chromatic Conundrum</h3>
                         <h5><a href="https://ernaniraffo.itch.io/chromatic-tower-defense"> Link to itch.io</a></h5>
+                        <h5><a href="https://github.com/lukaslicon/UnityWizards">Link to Github</a></h5>
                         <div className="gprojects-item">
                             <div className="gprojects-logo">
                                 <img src="assets/chromaticImage.png" alt="Chromatic" className="gprojects-logo-img" />
@@ -85,12 +87,14 @@ const App = () => {
                                     <h4>Contributions</h4>
                                 </div>
                                 <div className="gprojects-contributions">
-                                    <p>-I implemented the AI enemy pathfinding, UI/UX implementation, system interactions, and enemy spawning and wave system.</p>
+                                    <p>I implemented the AI enemy pathfinding, UI/UX implementation, system interactions, and enemy spawning and wave system.</p>
                                 </div>
                             </div>
                         </div>
                         <h3>Procedural City Generation</h3>
                         <h5><a href="https://github.com/Procedural-BananaSlug-Studios/PCG-City-Generator"> Link to Github</a></h5>
+
+                        
                         <div className="gprojects-item">
                             <div className="gprojects-logo">
                                 <img src="assets/proceduralcities.png" alt="ProcGen City" className="gprojects-logo-img" />
@@ -100,7 +104,9 @@ const App = () => {
                                     <h4>Project Description</h4>
                                 </div>
                                 <div className="gprojects-description">
-                                    <p>Lorem ipsum dolor sit amet. Et sint atque ut excepturi dolores et aspernatur quia qui aperiam reiciendis qui esse sequi ut enim error. Aut minus expedita vel consequuntur delectus aut vero dicta! Eum doloribus aliquam et adipisci voluptates quo mollitia repudiandae ut quae maiores.</p>
+                                    <p>This project, developed for CMPM 146, is a procedural city generator utilizing Unity and C#. The generator creates dynamic city layouts, leveraging the Wave Function Collapse (WFC) algorithm to produce varied and coherent cityscapes with roads, buildings, and blocks that follow logical patterns while maintaining randomness.</p>
+                                    <p>The PCG-City-Generator employs procedural generation techniques to generate cityscapes with randomized road networks, building placements, and city blocks. The project highlights advanced procedural content generation concepts using the Wave Function Collapse algorithm to ensure cities are varied yet cohesive.</p> 
+                                    <p>This project was developed to help game developers rapidly prototype mechanics in a random city environment based on their design constraints.</p>
                                 </div>
                             </div>
                             <div className="gprojects-box">
@@ -108,7 +114,8 @@ const App = () => {
                                     <h4>Contributions</h4>
                                 </div>
                                 <div className="gprojects-contributions">
-                                    <p>Lorem ipsum dolor sit amet. Et sint atque ut excepturi dolores et aspernatur quia qui aperiam reiciendis qui esse sequi ut enim error. Aut minus expedita vel consequuntur delectus aut vero dicta! Eum doloribus aliquam et adipisci voluptates quo mollitia repudiandae ut quae maiores.</p>
+                                    <p>I hooked up neighboring assets to properly allign with the design contraints. I also helped create the WaveFunctionCollapse.cs file which propagates constraints across the city grid, selecting valid tiles based on their compatibility with adjacent tiles.</p>
+
                                 </div>
                             </div>
                         </div>
@@ -125,8 +132,8 @@ const App = () => {
                         <h5><a href="https://github.com/lukaslicon/robotStateMachine"> Link to Github</a></h5>
                         <div className="iprojects-item">
                             <div className="iprojects-description">
-                                <p>Creating a ARPG like arena game with souls-like combat to practice gameplay programming fast paced melee combat systems. Spawners, AI pathfinding, loot system, upgrades, and more!
-                                </p>
+                                <p>Creating a ARPG like arena game with souls-like combat to practice gameplay programming fast paced melee combat systems. </p>
+                                <p>Practing Spawners, AI pathfinding, loot system, upgrades, and more!</p>
                             </div>
                             <div className="iprojects-logo">
                                 <img src="assets/imagesoon.png" alt="NavMesh Pathfinding" className="iprojects-logo-img" />
@@ -139,26 +146,24 @@ const App = () => {
                         <h5><a href="https://github.com/lukaslicon/robotStateMachine"> Link to Github</a></h5>
                         <div className="iprojects-item">
                             <div className="iprojects-description">
-                                <p>Lorem ipsum dolor sit amet. Et sint atque ut excepturi dolores et aspernatur quia qui aperiam reiciendis qui esse sequi ut enim error. Aut minus expedita vel consequuntur delectus aut vero dicta! Eum doloribus aliquam et adipisci voluptates quo mollitia repudiandae ut quae maiores.</p>
+                            <p>This project explores writing an application that operates both as a client and a server, communicating over a TCP socket connection using serialized messages. The server receives messages, deserializes them, and implements a simple state machine to control the behavior of a robot.</p>
+                            <p>In this assignment, the robot server receives serialized messages from the client, processes them, and controls the state of a robot based on the received messages. The robot alternates between sleeping and moving states according to the messages it receives.</p>
                             </div>
                         </div>
 
                         <h3>C++ Matrix operations calculator for sparse matrices</h3>
-                        <h5><a href="https://github.com/lukaslicon/robotStateMachine"> Link to Github</a></h5>
+                        <h5><a href="https://github.com/lukaslicon/MatrixOperations"> Link to Github</a></h5>
                         <div className="iprojects-item">
                             <div className="iprojects-description">
-                                <p>Lorem ipsum dolor sit amet. Et sint atque ut excepturi dolores et aspernatur quia qui aperiam reiciendis qui esse sequi ut enim error. Aut minus expedita vel consequuntur delectus aut vero dicta! Eum doloribus aliquam et adipisci voluptates quo mollitia repudiandae ut quae maiores.</p>
+                                <p>The purpose of this project is to implement a calculator that performs matrix operations, optimized for sparse matrices. The program is designed to exploit the expected sparseness of matrix operands, making it more efficient in terms of memory usage and computational performance. The main functionality is built around a custom implementation of a List Abstract Data Type (ADT), which is used to manipulate matrix data and perform shuffling operations. The project includes both the implementation and testing of the List ADT, along with the core program that performs the matrix operations.</p>
                             </div>
                         </div>
 
                         <h3>C++ Fractal Tile Renderer</h3>
-                        <h5><a href="https://github.com/lukaslicon/robotStateMachine"> Link to Github</a></h5>
+                        <h5><a href="https://github.com/lukaslicon/fractalTileMessage"> Link to Github</a></h5>
                         <div className="iprojects-item">
                             <div className="iprojects-description">
-                                <p> Created an asynchronous fractal renderer with a message queue system for task distribution among threads using std::async & ThreadSafeQueue. - Fractal Github Link • C++ Item Loader: Developed a JSON-based item loader system with inheritance & class properties, enabling support for custom items</p>
-                            </div>
-                            <div className="iprojects-logo">
-                                <img src="assets/crabity screenshot.png" alt="NavMesh Pathfinding" className="iprojects-logo-img" />
+                                <p>The Fractal Tiled Renderer generates a fractal image by dividing the image into tiles. Each tile is rendered asynchronously in parallel. Initially, std::async was used to manage parallel rendering of fractal tiles. However, this version refactors the program to utilize a thread-safe message queue with a fixed number of worker threads (e.g., 8 threads) for rendering the tiles. This project is intended to demonstrate concepts such as multithreading, thread-safe queues, and task distribution in C++.</p>
                             </div>
                         </div>
                         
@@ -167,10 +172,7 @@ const App = () => {
                         <h5><a href="https://github.com/lukaslicon/CMPM146-P1"> Link to Github</a></h5>
                         <div className="iprojects-item">
                             <div className="iprojects-description">
-                                <p>This Python project implements a bidirectional A* search algorithm to find paths in navmeshes created from user-provided images. The program takes an image and its corresponding navmesh representation as input and outputs an image showing the path from a source to a destination point defined interactively.</p>
-                            </div>
-                            <div className="iprojects-logo">
-                                <img src="assets/crabity screenshot.png" alt="NavMesh Pathfinding" className="iprojects-logo-img" />
+                                <p>This Python project implements a bidirectional A* search algorithm to find paths in navmeshes created from user-provided images. The program takes an image and its corresponding navmesh representation as input and outputs an image showing the path from a source to a destination point defined interactively. You can create your own test map by following the steps in the Github Readme file. </p>
                             </div>
                         </div>
 
@@ -181,9 +183,6 @@ const App = () => {
                             <div className="iprojects-description">
                                 <p>This project involves creating a bot that plays Ultimate Tic-Tac-Toe using Monte Carlo Tree Search (MCTS) in Python. Ultimate Tic-Tac-Toe is an advanced version of the classic Tic-Tac-Toe game where players play on a grid of 9 smaller Tic-Tac-Toe boards to complete a giant row, column, or diagonal. The MCTS bot will be evaluated through various experiments to ensure its effectiveness and performance.</p>
                             </div>
-                            <div className="iprojects-logo">
-                                <img src="assets/crabity screenshot.png" alt="NavMesh Pathfinding" className="iprojects-logo-img" />
-                            </div>
                         </div>
 
 
@@ -191,10 +190,7 @@ const App = () => {
                         <h5><a href="https://github.com/lukaslicon/P3-Behavior-Trees-for-Planet-Wars"> Link to Github</a></h5>
                         <div className="iprojects-item">
                             <div className="iprojects-description">
-                                <p>Lorem ipsum dolor sit amet. Et sint atque ut excepturi dolores et aspernatur quia qui aperiam reiciendis qui esse sequi ut enim error. Aut minus expedita vel consequuntur delectus aut vero dicta! Eum doloribus aliquam et adipisci voluptates quo mollitia repudiandae ut quae maiores.</p>
-                            </div>
-                            <div className="iprojects-logo">
-                                <img src="assets/crabity screenshot.png" alt="NavMesh Pathfinding" className="iprojects-logo-img" />
+                                <p>This project involves implementing a bot that plays Planet Wars using Behavior Trees in Python. Planet Wars is a real-time strategy game where the objective is to conquer a galaxy, planet by planet. Each planet produces ships per turn, which can be used to take over other planets from the enemy or neutral forces.</p>
                             </div>
                         </div>
                     </section>
